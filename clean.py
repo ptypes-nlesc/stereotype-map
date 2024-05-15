29d32164946ff1d63c4e3eb5fa44e8d2fe3068b7
Olga Lyashevska <olga@herenstraat.nl>
2024-05-15 11:25:43 +0200

    add first test

--- clean.py
+++ clean.py
@@ -64,9 +64,9 @@ def flatten_tags(tags):
     return df_flat_tag
 
 
-def get_popular_tags(df_flat_tag, quantile=0.75):
+def get_tag_counts(df_flat_tag):
     """
-    Gets the popular tags from a DataFrame based on quantile.
+    Gets the counts of each tag from a DataFrame.
 
     Parameters
     ----------
@@ -76,17 +76,34 @@ def get_popular_tags(df_flat_tag, quantile=0.75):
     Returns
     -------
         DataFrame
-            a DataFrame sorted by the counts of each tag in descending order
+            a DataFrame sorted by the counts of each tag in descending order with their counts
     """
-    popular_tags = (
+    return (
         df_flat_tag.groupby("tag")
         .size()
         .reset_index(name="counts")  # type: ignore
         .sort_values("counts", ascending=False)
         .reset_index(drop=True)
     )
-    min_appearance = popular_tags.counts.quantile(quantile)
-    return set(popular_tags[popular_tags.counts >= min_appearance]["tag"])
+
+
+def get_popular_tags(df, quantile=0.75):
+    """
+    Gets the popular tags from a DataFrame based on quantile.
+
+    Parameters
+    ----------
+        df : DataFrame
+            DataFrame with a single column named "tag" containing the tags
+
+    Returns
+    -------
+        DataFrame
+            a DataFrame sorted by the counts of each tag in descending order
+    """
+    tag_counts = get_tag_counts(df)
+    min_appearance = tag_counts.counts.quantile(quantile)
+    return set(tag_counts[tag_counts.counts >= min_appearance]["tag"])
 
 
 def filter_popular_tags(tag_list, popular_tags_set):
@@ -110,26 +127,26 @@ def filter_popular_tags(tag_list, popular_tags_set):
 
 if __name__ == "__main__":
     # Read the data
-    df = pd.read_csv("data/porn-with-dates-2022.csv")
+    dat = pd.read_csv("data/porn-with-dates-2022.csv")
 
     # Extract tags
-    df["tags"] = df["categories"].apply(extract_tags)
+    dat["tags"] = dat["categories"].apply(extract_tags)
 
     # Remove unwanted tag
-    df["tags"] = df["tags"].apply(remove_tag)
+    dat["tags"] = dat["tags"].apply(remove_tag)
 
     # Flatten tags into a DataFrame
-    df_flat_tag = flatten_tags(df["tags"])
+    dat_flat_tag = flatten_tags(dat["tags"])
 
     # Get popular tags
-    popular_tags = get_popular_tags(df_flat_tag)
+    popular_tags = get_popular_tags(dat_flat_tag)
 
     # Filter tags based on popular tags
-    df["popular_tags"] = df["tags"].apply(
+    dat["popular_tags"] = dat["tags"].apply(
         lambda tag_list: filter_popular_tags(tag_list, popular_tags)
     )
 
     # Filter DataFrame to include only rows where 'popular_tags' is not empty
-    df_popular_tags = df.loc[df["popular_tags"].apply(lambda tag_list: tag_list != [])]
-
-    print(df_popular_tags.head())
+    dat_popular_tags = dat.loc[dat["popular_tags"].apply(lambda tag_list: tag_list != [])]
+    dat_popular_tags.to_csv("data/dat.csv", index=False)
+    print(dat_popular_tags.head())
---
+++ tests/test_extract_tags.py
@@ -0,0 +1,9 @@
+import pytest
+
+from clean import extract_tags
+
+
+def test_extract_tags():
+    categories = "['tag1', 'tag2', 'tag3']"
+    expected_output = ["tag1", "tag2", "tag3"]
+    assert extract_tags(categories) == expected_output
