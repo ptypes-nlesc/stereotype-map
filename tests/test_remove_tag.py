4293e4e263a789830789b67b0f3f9f9f8199296c
Olga Lyashevska <olga@herenstraat.nl>
2024-05-15 13:31:49 +0200

    test remove tag

---
+++ tests/test_remove_tag.py
@@ -0,0 +1,9 @@
+from clean import remove_tag
+
+
+def test_remove_tag():
+    tag_list = ["tag1", "tag2", "HD Porn", "tag3"]
+    tag_to_remove = "HD Porn"
+    expected_output = ["tag1", "tag2", "tag3"]
+    assert remove_tag(tag_list, tag_to_remove) == expected_output
+
