9acffbe159f0edd07b14c5a23a2be60b7aefbbd8
Olga Lyashevska <olga.lyashevska@gmail.com>
2024-06-17 16:57:37 +0200

    Add similarity graph

--- embeddings.py
+++ embeddings.py
@@ -6,6 +6,9 @@ Steps:
 3. similarity calculation using cosine similarity,
 4. visualization?
 """
+# TODO Write tests
+# TODO Write documentation
+# TODO Add type hints
 
 import json
 
@@ -66,4 +69,41 @@ plt.title("Cosine Similarity between Videos and Stereotypes")
 plt.xlabel("Stereotypes")
 plt.ylabel("Videos")
 plt.savefig("plots/video_sterotype_heatmap.png")
+plt.show()
+
+
+import networkx as nx
+
+# Create a graph
+G = nx.Graph()
+# Add nodes for tags and stereotypes
+G.add_nodes_from(tags.keys(), bipartite=0)
+G.add_nodes_from(stereotypes.keys(), bipartite=1)
+
+# Add edges with weights based on similarity
+for i, tag in enumerate(tags.keys()):
+    for j, stereotype in enumerate(stereotypes.keys()):
+        weight = sim[i, j]
+        if weight > 0.3:  # Only add edges with significant similarity
+            G.add_edge(tag, stereotype, weight=weight)
+
+# Position nodes using bipartite layout
+pos = nx.spring_layout(G, k=0.5, iterations=50)
+
+# Draw the graph
+plt.figure(figsize=(12, 8))
+edges = G.edges(data=True)
+weights = [edge[2]["weight"] for edge in edges]
+nx.draw(
+    G,
+    pos,
+    with_labels=True,
+    node_size=3000,
+    node_color="skyblue",
+    font_size=10,
+    font_weight="bold",
+)
+nx.draw_networkx_edges(G, pos, edge_color=weights, edge_cmap=plt.cm.Blues, width=2)
+plt.title("Network Graph of Cosine Similarity between Tags and Stereotypes")
+plt.savefig("plots/video_sterotype_graph.png")
 plt.show()
