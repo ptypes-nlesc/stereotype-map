01e5afb1460d0da1ebae857fd3644f1f632cb7a3
Olga Lyashevska <olga.lyashevska@gmail.com>
2024-07-10 14:33:48 +0200

    Prepare files for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'stereotype-map'
-copyright = '2024, Olga Lyashevska'
-author = 'Olga Lyashevska'
-release = 'pre-alpha'
+project = "stereotype-map"
+copyright = "2024, Olga Lyashevska"
+author = "Olga Lyashevska"
+release = "0.1"
+version = "0.1.0"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+]
 
-extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
