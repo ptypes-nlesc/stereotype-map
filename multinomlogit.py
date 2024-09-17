f242575ce3b5249a9d6bf17a78c1b42bc0a94d92
Olga Lyashevska <olga.lyashevska@gmail.com>
2024-09-17 15:36:12 +0200

    add helper

--- multinomlogit.py
+++ multinomlogit.py
@@ -1,8 +1,18 @@
+import helpers
 import embeddings
+import pandas as pd
+
+# load data
+dat = pd.read_csv("data/dat.csv")
+
+# prepare tags
+helpers.df_to_json_string(dat.head(20), 0, -1)
 
 embeddings.download_nltk_data()
+# load stereotypes
 stereotypes = embeddings.load_and_preprocess_data("stereotypes.json")
-tags = embeddings.load_and_preprocess_data("tags.json")
+tags = embeddings.load_and_preprocess_data("video-tags.json")
+
 stereotypes_emb = embeddings.generate_embeddings(stereotypes)
 tags_emb = embeddings.generate_embeddings(tags)
 sim_df = embeddings.calculate_similarity(tags_emb, stereotypes_emb)
