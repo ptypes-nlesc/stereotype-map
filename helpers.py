cdefbd848d72bd26aa2a5e1bdf4543c129a84ea3
Olga Lyashevska <olga.lyashevska@gmail.com>
2024-09-24 13:05:59 +0200

    prepare tags in req format

--- helpers.py
+++ helpers.py
@@ -1,9 +1,11 @@
-import pandas as pd
 import json
 
-def df_to_json_string(df, key_col_index, value_col_index):
+import pandas as pd
+
+
+def df_to_json(df, key_col_index, value_col_index, filename="output.json"):
     """
-    Convert specified columns of a DataFrame to a JSON string.
+    Convert specified columns of a DataFrame to a JSON string and saves it to a file.
 
     This function takes a DataFrame and two column indices, converts the specified columns
     to a dictionary where one column's values are used as keys and the other column's values
@@ -13,17 +15,17 @@ def df_to_json_string(df, key_col_index, value_col_index):
     df (pd.DataFrame): The DataFrame containing the data.
     key_col_index (int): The index of the column to be used as keys in the JSON.
     value_col_index (int): The index of the column to be used as values in the JSON.
+    filename (str): The name of the file to save the JSON data.
 
     Returns:
-    str: A JSON string representing the DataFrame.
+    None
 
     Example:
     >>> df = pd.DataFrame({
     >>>     'col1': ['Video1', 'Video2'],
     >>>     'col2': [['Tag1', 'Tag2', 'Tag3'], ['Tag3', 'Tag4']]
     >>> })
-    >>> df_to_json_string(df, 0, 1)
-    '{"Video1": "Tag1, Tag2, Tag3", "Video2": "Tag3, Tag4"}'
+    >>> df_to_json_file(df, 0, 1, 'output.json')
     """
     # Ensure the column indices are within the DataFrame's bounds
     if key_col_index >= len(df.columns) or value_col_index >= len(df.columns):
@@ -31,20 +33,24 @@ def df_to_json_string(df, key_col_index, value_col_index):
 
     # Create a dictionary with the specified columns as keys and comma-separated string values
     result_dict = {
-        row[key_col_index]: ', '.join(row[value_col_index]) for index, row in df.iterrows()
+        row[key_col_index]: ", ".join(row[value_col_index])
+        for index, row in df.iterrows()
     }
 
     # Convert the dictionary to a JSON string
     json_str = json.dumps(result_dict, ensure_ascii=False, indent=4)
 
-    return json_str
+    # Save the JSON string to a file
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(json_str)
+
 
 if __name__ == "__main__":
+    # Example usage
     df = pd.DataFrame(
         {
             "col1": ["Video1", "Video2"],
-            "col2": [["Tag1", "Tag2", "Tag3"], ["Tag3", "Tag4"]]
+            "col2": [["Tag1", "Tag2", "Tag3"], ["Tag3", "Tag4"]],
         }
     )
-    json_output = df_to_json_string(df, 0, 1)
-    print(json_output)
+    df_to_json(df, 0, 1, "output.json")
--- multinomlogit.py
+++ multinomlogit.py
@@ -1,17 +1,30 @@
-import helpers
-import embeddings
+import importlib
+
 import pandas as pd
+
+import clean
+import embeddings
+import helpers
+
+importlib.reload(helpers)
 
 # load data
 dat = pd.read_csv("data/dat.csv")
 
+dat["popular_tags_formatted"] = dat["popular_tags"].apply(clean.extract_tags)
 # prepare tags
-helpers.df_to_json_string(dat.head(20), 0, -1)
+helpers.df_to_json(
+    df=dat,
+    key_col_index=0,
+    value_col_index=-1,
+    filename="video-tags.json"
+)
+
 
 embeddings.download_nltk_data()
 # load stereotypes
 stereotypes = embeddings.load_and_preprocess_data("stereotypes.json")
-tags = embeddings.load_and_preprocess_data("video-tags.json")
+tags = embeddings.load_and_preprocess_data("output.json")
 
 stereotypes_emb = embeddings.generate_embeddings(stereotypes)
 tags_emb = embeddings.generate_embeddings(tags)
